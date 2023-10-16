8335d0bd00b74ab355bb7bf3907d79c8238105d7
plaul <lmor@kea.dk>
2023-10-16 13:25:29 +0200

    ready to commit

---
+++ pages/cars/cars.js
@@ -0,0 +1,21 @@
+import { API_URL } from "../../settings.js"
+const URL = API_URL + "/cars/admin"
+import {sanitizeStringWithTableRows,makeOptions} from "../../utils.js"
+
+export async function initCars() {
+  //const cars = await fetch(URL).then((res)=>handleHttpErrors(res))
+  const cars = await fetch(URL,makeOptions("GET",null,true)).then(res => res.json())
+
+  const tableRows = cars.map(car => `
+  <tr>
+  <td>${car.id}</td>
+  <td>${car.brand}</td>
+  <td>${car.model}</td>
+  <td>${car.pricePrDay}</td>
+  <td>${car.bestDiscount}</td>
+  </tr>
+  `)
+  const tableRowsAsStr = tableRows.join("")
+
+  document.getElementById("table-rows").innerHTML = sanitizeStringWithTableRows(tableRowsAsStr)
+}
